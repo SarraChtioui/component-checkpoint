b20a7164241eba08ddfa31e1387826c6c5ab2c3c
SarraChtioui <schconsultingtn@gmail.com>
2020-12-04 20:42:33 +0100

    component checkpoint

---
+++ src/component/profile/Address.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Address() {
+    return (
+        <div>
+           <h3>Beverly Allitt
+Rampton Hospital 
+Woodbeck
+Nottinghamshire
+DN22 0PD 
+England </h3> 
+        </div>
+    )
+}
+
+export default Address
